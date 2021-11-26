cceb9cb4773c8e3d2225f413f7f4f54dd6f8863d
AChavez <50527351+AAgustinChavez@users.noreply.github.com>
2021-11-26 02:56:02 -0600

    liquidity_pull_refresh

--- lib/create_file_img.dart
+++ lib/create_file_img.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:doc_scanner/api/firebase_api.dart';
 import 'package:doc_scanner/auth/bloc/auth_bloc.dart';
 import 'package:doc_scanner/imagesFiles/imagefilter.dart';
@@ -9,6 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'dart:io';
 import 'package:photofilters/photofilters.dart';
 import 'package:image/image.dart' as imageLib;
@@ -32,7 +36,25 @@ class _CreateFileIMGState extends State<CreateFileIMG> {
     final User? user = auth.currentUser;
     final uid = user!.uid;
     futureFiles = FirebaseApi.listAll('documents/' + uid + '/');
-    
+  }
+
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Future _handleRefresh(){
+    final Completer<void> completer = Completer<void>();
+
+    Timer(const Duration(seconds: 1), () {
+      completer.complete();
+    });
+
+    return completer.future.then<void>((futureFiles){
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text("Refresh complete"),
+          )
+        );
+    });
   }
 
   Widget buildFile(BuildContext context, FirebaseFile file) => ListTile(
@@ -77,6 +99,7 @@ class _CreateFileIMGState extends State<CreateFileIMG> {
         ),
       );
 
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -119,64 +142,74 @@ class _CreateFileIMGState extends State<CreateFileIMG> {
               ],
             ),
             body: FutureBuilder<List<FirebaseFile>>(
-              future: futureFiles,
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    return Center(child: CircularProgressIndicator());
-                  default:
-                    if (snapshot.hasError) {
-                      return const Center(child: Text('Some error occurred!'));
-                    } else {
-                      final files = snapshot.data!;
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          buildHeader(files.length),
-                          const SizedBox(height: 12),
-                          Expanded(
-                            child: ListView.builder(
-                              itemCount: files.length,
-                              itemBuilder: (context, index) {
-                                final file = files[index];
-
-                                return buildFile(context, file);
-                              },
-                            ),
-                          ),
-                          Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                ListView(
-                                  shrinkWrap: true,
-                                  padding: const EdgeInsets.all(20),
-                                  children: [
-                                    ButtonBar(
-                                        alignment: MainAxisAlignment.center,
-                                        children: [
-                                          IconButton(
-                                              onPressed: () async {
-                                                Navigator.push(
-                                                    context,
-                                                    MaterialPageRoute(
-                                                        builder: (context) =>
-                                                            const ImageFilter()));
-                                              },
-                                              icon: Icon(Icons.scanner,
-                                                  color: Colors.cyan, size: 50))
-                                        ]),
-                                  ],
+                  future: futureFiles,
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                        return Center(child: CircularProgressIndicator());
+                      default:
+                        if (snapshot.hasError) {
+                          return const Center(child: Text('Some error occurred!'));
+                        } else {
+                          final files = snapshot.data!;
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              buildHeader(files.length),
+                              const SizedBox(height: 12),
+                              Expanded(
+                                child: LiquidPullToRefresh(
+                                  key: _refreshIndicatorKey,
+                                  onRefresh: _handleRefresh,
+                                  child: ListView.builder(
+                                    itemCount: files.length,
+                                    itemBuilder: (context, index) {
+                                      final file = files[index];
+                                              
+                                      return buildFile(context, file);
+                                        
+                                    },
+                                  ),
                                 ),
-                              ]),
-                        ],
-                      );
+                             
+                              ),
+                              Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    ListView(
+                                      shrinkWrap: true,
+                                      padding: const EdgeInsets.all(20),
+                                      children: [
+                                        ButtonBar(
+                                            alignment: MainAxisAlignment.center,
+                                            children: [
+                                              IconButton(
+                                                  onPressed: () async {
+                                                    Navigator.push(
+                                                        context,
+                                                        MaterialPageRoute(
+                                                            builder: (context) =>
+                                                                const ImageFilter()));
+                                                  },
+                                                  icon: Icon(Icons.scanner,
+                                                      color: Colors.cyan, size: 50))
+                                            ]),
+                                      ],
+                                    ),
+                                  ]),
+                            ],
+                          );
+                        }
                     }
-                }
-              },
-            ),
+                  },
+                ),
+            
+            
           );
         },
       ),
     );
   }
 }
+
+
